c8065f60dd11c67e43d27745d6255dbd6445dd95
Code Sandbox <hello@codesandbox.io>
2023-06-14 04:46:30 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.630fcedd47341b581c25b2c81fb1b551.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cded5bad9e813c541c73216b3acf72d4",
+    "url": "https://suhaib33.github.io/csb-6fytnc/index.html"
+  },
+  {
+    "revision": "a7592936c650e2395c32",
+    "url": "https://suhaib33.github.io/csb-6fytnc/static/js/2.e06bb6bb.chunk.js"
+  },
+  {
+    "revision": "226fe25c0bb16e6b2be4",
+    "url": "https://suhaib33.github.io/csb-6fytnc/static/js/main.6541f3ca.chunk.js"
+  },
+  {
+    "revision": "e9900168dafd92de5711",
+    "url": "https://suhaib33.github.io/csb-6fytnc/static/js/runtime-main.91a9d1b8.js"
+  }
+]);
